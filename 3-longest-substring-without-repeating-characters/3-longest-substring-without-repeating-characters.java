d99c2f48d6946144213efcff307093e73e2364b8
Julian Lopez <66705096+julianlopez6850@users.noreply.github.com>
2022-08-29 01:40:46 -0400

    Time: 681 ms (5.01%), Space: 145.2 MB (5.50%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -7,25 +7,7 @@ class Solution {
         
         int currStringStartIndex = 0;
         int repeatCharIndex = 0;
-        /*
-        for(int i = 0; i < s.length(); i++)
-        {
-            for(int j = i; j < s.length(); j++)
-            {
-                if(!currString.contains(Character.toString(s.charAt(j))))
-                    currString += s.charAt(j);
-                else
-                {
-                    currString = "";
-                    break;
-                }
-                
-                if(currString.length() > longest.length())
-                {
-                    longest = currString;
-                }
-            }
-        }*/
+        
         
         for(int i = 0; i < s.length(); i++)
         {
