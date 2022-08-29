ba66a6a4ece355e13fff06e5434b4400322941a3
Julian Lopez <66705096+julianlopez6850@users.noreply.github.com>
2022-08-29 01:35:01 -0400

    Time: 796 ms (5.01%), Space: 145.2 MB (5.50%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -5,7 +5,9 @@ class Solution {
         String currString = "";
         String longest = "";
         
-        
+        int currStringStartIndex = 0;
+        int repeatCharIndex = 0;
+        /*
         for(int i = 0; i < s.length(); i++)
         {
             for(int j = i; j < s.length(); j++)
@@ -23,8 +25,26 @@ class Solution {
                     longest = currString;
                 }
             }
-        }
+        }*/
         
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(currString == "")
+                currStringStartIndex = i;
+            
+            if(!currString.contains(Character.toString(s.charAt(i))))
+                currString += s.charAt(i);
+            else
+            {
+                i = currString.indexOf(s.charAt(i)) + currStringStartIndex;
+                currString = "";
+            }
+
+            if(currString.length() > longest.length())
+            {
+                longest = currString;
+            }
+        }
         return longest.length();
     }
 }
