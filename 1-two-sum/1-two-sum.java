d955c8a1617757773d09cd0c023efd58a01fdd51
Julian Lopez <66705096+julianlopez6850@users.noreply.github.com>
2022-08-28 20:06:00 -0400

    Time: 66 ms (38.94%), Space: 42.3 MB (96.70%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -5,7 +5,6 @@ class Solution {
         
         for(int i = 0; i < nums.length - 1; i++)
         {
-            
             for(int j = i + 1; j < nums.length; j++)
             {
                 if(nums[i] + nums[j] == target)
@@ -15,7 +14,6 @@ class Solution {
                     break;
                 }
             }
-            
         }
         return indeces;
     }
