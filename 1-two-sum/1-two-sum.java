c68380cec8776578288340c2462321a35899cce9
Julian Lopez <66705096+julianlopez6850@users.noreply.github.com>
2022-08-28 20:05:15 -0400

    Time: 70 ms (36.80%), Space: 44.9 MB (70.15%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -5,6 +5,7 @@ class Solution {
         
         for(int i = 0; i < nums.length - 1; i++)
         {
+            
             for(int j = i + 1; j < nums.length; j++)
             {
                 if(nums[i] + nums[j] == target)
@@ -14,6 +15,7 @@ class Solution {
                     break;
                 }
             }
+            
         }
         return indeces;
     }
