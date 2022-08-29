e8a38067d615e236b12eaed917286e63adec47eb
Julian Lopez <66705096+julianlopez6850@users.noreply.github.com>
2022-08-29 00:38:45 -0400

    Time: 11 ms (17.21%), Space: 50.3 MB (27.25%) - LeetHub

---
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
@@ -0,0 +1,57 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        
+        int[] combined = new int[nums1.length + nums2.length];
+        
+        int index1 = 0;
+        int index2 = 0;
+        
+        for(int i = 0; i < nums1.length + nums2.length; i++)
+        {
+            if(index1 >= nums1.length)
+            {
+                combined[i] = nums2[index2];
+                index2++;
+            }
+            else if(index2 >= nums2.length)
+            {
+                combined[i] = nums1[index1];
+                index1++;
+            }  
+            else if(nums1[index1] <= nums2[index2])
+            {
+                combined[i] = nums1[index1];
+                index1++;
+            }
+            else
+            {
+                combined[i] = nums2[index2];
+                index2++;
+            }
+        }
+        
+        double output = 0;
+        
+        System.out.println(combined.length);
+        
+        //for(int i = 0; i < combined.length; i++)
+        //    System.out.println(combined[i]);
+        
+        if(combined.length % 2 == 1)
+            output = combined[combined.length / 2];
+        else
+            output = (combined[combined.length / 2 - 1] + combined[combined.length / 2]) / 2.0;
+        /*
+        System.out.println(combined.length / 2 - 1);
+        System.out.println(combined[combined.length / 2 - 1]);
+        
+        System.out.println(combined.length / 2);
+        System.out.println(combined[combined.length / 2]);
+        
+        System.out.println((combined[combined.length / 2 - 1] + combined[combined.length / 2]) / 2.0);*/
+        
+        return output;
+        
+        
+    }
+}
