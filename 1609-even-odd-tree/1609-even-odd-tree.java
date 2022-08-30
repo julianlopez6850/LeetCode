e6413d4163cebf962093adc9d1a6dfea3b62337e
Julian Lopez <66705096+julianlopez6850@users.noreply.github.com>
2022-08-30 06:48:21 -0400

    Time: 46 ms (14.78%), Space: 92.1 MB (17.41%) - LeetHub

--- 1609-even-odd-tree/1609-even-odd-tree.java
+++ 1609-even-odd-tree/1609-even-odd-tree.java
@@ -24,9 +24,6 @@ class Solution {
         queue.add(null);
         levelOrderTraversal(root);
         
-        System.out.println(list.size());
-        System.out.println(list);
-        
         int prevValue = 0;
         int currValue = 0;
         
@@ -39,22 +36,13 @@ class Solution {
             {
                 evenLevel = !evenLevel;
                 firstValueInLevel = true;
-                
-                System.out.println(evenLevel);
             }
-            
-            System.out.println(currValue);
-            
             if(list.get(i) != null)
             {
                 currValue = list.get(i);
             
-                
                 if((evenLevel && currValue % 2 == 0) || (!evenLevel && currValue % 2 == 1))
-                {
-                    System.out.println("FALSE: even level and even number OR odd level and odd number" + evenLevel + currValue);
                     return false;
-                }
                 else if(firstValueInLevel)
                 {
                     prevValue = currValue;
@@ -63,26 +51,16 @@ class Solution {
                 else if(evenLevel)
                 {
                     if(currValue <= prevValue || currValue % 2 != 1)
-                    {
-                        System.out.println("FALSE: " + prevValue + ">=" + currValue);
                         return false;
-                    }
                     else
-                    {
                         prevValue = currValue;
-                    }
                 }
                 else if(!evenLevel)
                 {
                     if(currValue >= prevValue || currValue % 2 != 0)
-                    {
-                        System.out.println("FALSE: " + prevValue + "<=" + currValue);
                         return false;
-                    }
                     else
-                    {
                         prevValue = currValue;
-                    }
                 }
             }
         }
@@ -90,9 +68,6 @@ class Solution {
     }
     
     public List<Integer> levelOrderTraversal(TreeNode root) {
-        
-        System.out.println(root);
-        
         if(root != null)
         {
             list.add(root.val);
@@ -101,18 +76,13 @@ class Solution {
             if(root.right != null)
                 queue.add(root.right);
         }
-        else
+        else if(queue.size() > 0)
         {
-            if(queue.size() > 0)
-            {
-                list.add(null);
-                queue.add(null);
-            }
+            list.add(null);
+            queue.add(null);
         }
         if(queue.size() > 0)
-        {
             levelOrderTraversal(queue.remove());
-        }
         return list;
     }
 }
