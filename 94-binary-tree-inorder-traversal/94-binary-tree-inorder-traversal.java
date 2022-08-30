03bbfad662834bf35d17154b4e8ffd22d9ae3f86
Julian Lopez <66705096+julianlopez6850@users.noreply.github.com>
2022-08-30 03:06:52 -0400

    Time: 0 ms (100.00%), Space: 40.4 MB (93.55%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -20,9 +20,11 @@ class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         if(root != null)
         {
-            inorderTraversal(root.left);
+            if(root.left != null)
+                inorderTraversal(root.left);
             list.add(root.val);
-            inorderTraversal(root.right);
+            if(root.right != null)
+                inorderTraversal(root.right);
         }
         return list;
     }
