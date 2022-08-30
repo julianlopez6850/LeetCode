39e0d802894e9c8acfa01e3c8adcfb0af2843ac2
Julian Lopez <66705096+julianlopez6850@users.noreply.github.com>
2022-08-30 17:46:34 -0400

    Time: 3 ms (29.20%), Space: 39.9 MB (88.62%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.java
+++ 7-reverse-integer/7-reverse-integer.java
@@ -24,19 +24,15 @@ class Solution {
         
         int sign = 1;
         if(x < 0)
-        {
             sign = -1;
-            x = Math.abs(x);
-        }
+        x = Math.abs(x);
         while(x > 0)
         {
             stack.push(x % 10);
             x /= 10;
         }
         for(int i = 0; stack.size() > 0; i++)
-        {
             reversed += (int)Math.pow(10, i) * stack.pop();
-        }
         reversed *= sign;
         
         return reversed;
