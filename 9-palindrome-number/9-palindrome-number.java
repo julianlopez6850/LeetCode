82454d40d5d860124d9c9f7e8c8f6ab5565c914d
Julian Lopez <66705096+julianlopez6850@users.noreply.github.com>
2022-08-30 21:12:30 -0400

    Time: 27 ms (11.99%), Space: 45.5 MB (24.59%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    
+    Stack<Integer> stack = new Stack<Integer>();
+    
+    public boolean isPalindrome(int x) {
+        if(x < 0 || x == Integer.MAX_VALUE)
+            return false;
+        
+        int num = x;
+        int reversed = 0;
+        
+        for(int i = 0; x > 0; i++)
+        {
+            stack.push(x % 10);
+            x /= 10;
+        }
+        for(int i = 0; stack.size() > 0; i++)
+        {
+            reversed += stack.pop() * Math.pow(10, i);
+        }
+        if(num == reversed)
+            return true;
+        return false;
+    }
+}
