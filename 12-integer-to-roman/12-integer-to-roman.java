b1bd1e26f03e18e5cca0e1561150de701dff5adf
Julian Lopez <66705096+julianlopez6850@users.noreply.github.com>
2022-08-30 22:00:35 -0400

    Time: 9 ms (64.35%), Space: 46.2 MB (35.73%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.java
+++ 12-integer-to-roman/12-integer-to-roman.java
@@ -3,73 +3,71 @@ class Solution {
         
         String roman = "";
         
-        while(num > 0)
+        
+        while(num >= 1000)
         {
-            if(num >= 1000)
-            {
-                roman += "M";
-                num -= 1000;
-            }
-            else if(num >= 900)
-            {
-                roman += "CM";
-                num -= 900;
-            }
-            else if(num >= 500)
-            {
-                roman += "D";
-                num -= 500;
-            }
-            else if(num >= 400)
-            {
-                roman += "CD";
-                num -= 400;
-            }
-            else if(num >= 100)
-            {
-                roman += "C";
-                num -= 100;
-            }
-            else if(num >= 90)
-            {
-                roman += "XC";
-                num -= 90;
-            }
-            else if(num >= 50)
-            {
-                roman += "L";
-                num -= 50;
-            }
-            else if(num >= 40)
-            {
-                roman += "XL";
-                num -= 40;
-            }
-            else if(num >= 10)
-            {
-                roman += "X";
-                num -= 10;
-            }
-            else if(num >= 9)
-            {
-                roman += "IX";
-                num -= 9;
-            }
-            else if(num >= 5)
-            {
-                roman += "V";
-                num -= 5;
-            }
-            else if(num >= 4)
-            {
-                roman += "IV";
-                num -= 4;
-            }
-            else if(num >= 1)
-            {
-                roman += "I";
-                num -= 1;
-            }
+            roman += "M";
+            num -= 1000;
+        }
+        while(num >= 900)
+        {
+            roman += "CM";
+            num -= 900;
+        }
+        while(num >= 500)
+        {
+            roman += "D";
+            num -= 500;
+        }
+        while(num >= 400)
+        {
+            roman += "CD";
+            num -= 400;
+        }
+        while(num >= 100)
+        {
+            roman += "C";
+            num -= 100;
+        }
+        while(num >= 90)
+        {
+            roman += "XC";
+            num -= 90;
+        }
+        while(num >= 50)
+        {
+            roman += "L";
+            num -= 50;
+        }
+        while(num >= 40)
+        {
+            roman += "XL";
+            num -= 40;
+        }
+        while(num >= 10)
+        {
+            roman += "X";
+            num -= 10;
+        }
+        while(num >= 9)
+        {
+            roman += "IX";
+            num -= 9;
+        }
+        while(num >= 5)
+        {
+            roman += "V";
+            num -= 5;
+        }
+        while(num >= 4)
+        {
+            roman += "IV";
+            num -= 4;
+        }
+        while(num >= 1)
+        {
+            roman += "I";
+            num -= 1;
         }
         return roman;
     }
