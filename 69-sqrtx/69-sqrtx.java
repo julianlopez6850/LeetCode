d6e318598b606b747f9341b6ee26fedf3f9f1007
Julian Lopez <66705096+julianlopez6850@users.noreply.github.com>
2022-09-01 18:34:35 -0400

    Time: 15 ms (14.56%), Space: 41.9 MB (17.64%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,10 +1,25 @@
 class Solution {
     public int mySqrt(int x) {
-        for(int i = 0; i < 46341; i++)  // 46341 is the maximum sqrt
+        for(int i = mySqrtHelper(x, 1); i < 46341; i++)
         {
             if(i * i > x)
                 return i - 1;
         }
         return 46340;
+    }
+    
+    public int mySqrtHelper(double x, int sqrt)
+    {
+        if(x / 9 > 1)
+        {
+            sqrt *= 3;
+            return mySqrtHelper(x / 9, sqrt);
+        }
+        if(x / 4 > 1)
+        {
+            sqrt *= 2;
+            return mySqrtHelper(x / 4, sqrt);
+        }
+        return (int)(sqrt);
     }
 }
