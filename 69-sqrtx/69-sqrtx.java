3d6d76ff11bf1543bb9d6d0f9ec0565cfc5c6fda
Julian Lopez <66705096+julianlopez6850@users.noreply.github.com>
2022-09-01 21:39:48 -0400

    Time: 2 ms (83.50%), Space: 41.4 MB (52.76%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,17 +1,21 @@
 class Solution {
+    
+    
+    
     public int mySqrt(int x)
     {
-        return mySqrtHelper(x, 0, 46341);
-    }
-    
-    public int mySqrtHelper(double x, int startIndex, int endIndex)
-    {
-        if(endIndex - startIndex == 1)
-            return startIndex;
-        int midIndex = endIndex - (endIndex - startIndex) / 2;
-        if(midIndex * midIndex > x)
-            return mySqrtHelper(x, startIndex, midIndex);
-        else
-            return mySqrtHelper(x, midIndex, endIndex);
+        int startIndex = 0;
+        int endIndex = 46341;
+        
+        while(true)
+        {
+            if(endIndex - startIndex == 1)
+                return startIndex;
+            int midIndex = endIndex - (endIndex - startIndex) / 2;
+            if(midIndex * midIndex > x)
+                endIndex = midIndex;
+            else
+                startIndex = midIndex;
+        }
     }
 }
