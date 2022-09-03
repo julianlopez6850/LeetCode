0620a4be47b5f84003ce57504dd6c4bb6724a0d1
Julian Lopez <66705096+julianlopez6850@users.noreply.github.com>
2022-09-03 18:19:30 -0400

    Time: 2 ms (95.27%), Space: 41.9 MB (97.48%) - LeetHub

---
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
@@ -0,0 +1,46 @@
+class Solution {
+    
+    public int[] numsSameConsecDiff(int n, int k)
+    {
+        if(n == 1)
+            return new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        
+        List<Integer> result = new ArrayList<Integer>();
+        for(int i = 1; i < 10; i++)
+            DepthFirstSearch(n, i, k, result);
+        
+        int[] resultArray = new int[result.size()];
+        for(int i = 0; i < result.size(); i++)
+            resultArray[i] = result.get(i);
+            
+        return resultArray;
+    }
+    
+    public void DepthFirstSearch(int n, int num, int k, List<Integer> result)
+    {
+        if(n == 1)              // if there are no more digits to be added, return.
+        {
+            result.add(num);
+            return;
+        }
+        List<Integer> nextDigits = new ArrayList<Integer>();
+        
+        int lastDigit = num % 10;
+        
+        /*  
+            if lastDigit + k < 10, it is a possible next digit, thus add it to nextDigit.
+            if lastDigit - k > 0, it is a possible next digit, thus add it to nextDigit.
+            if k = 0, make sure to not add the digit twice.
+        */
+        if(lastDigit + k < 10)
+            nextDigits.add(lastDigit + k);
+        if(k != 0 && lastDigit - k >= 0)
+            nextDigits.add(lastDigit - k);
+        
+        for(int i = 0; i < nextDigits.size(); i++)      // repeat method call with newNum (num with nextDigits appended).
+        {
+            int newNum = num * 10 + nextDigits.get(i);
+            DepthFirstSearch(n - 1, newNum, k, result);
+        }
+    }
+}
