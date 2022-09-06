ff326f3090b343887e5d87d4b125e745700aa85f
Julian Lopez <66705096+julianlopez6850@users.noreply.github.com>
2022-09-06 00:54:32 -0400

    Time: 0 ms (100.00%), Space: 41.9 MB (40.79%) - LeetHub

---
+++ 814-binary-tree-pruning/814-binary-tree-pruning.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode pruneTree(TreeNode root) {
+        if(root != null)
+        {
+            if(root.left != null)
+            {
+                pruneTree(root.left);
+                if(root.left.val == -1)
+                    root.left = null;
+            }
+            if(root.right != null)
+            {
+                pruneTree(root.right);
+                if(root.right.val == -1)
+                    root.right = null;
+            }
+            if(root.left == null && root.right == null && root.val == 0)
+                root.val = -1;
+        }
+        if(root.val == -1)
+            return null;
+        return root;
+    }
+}
