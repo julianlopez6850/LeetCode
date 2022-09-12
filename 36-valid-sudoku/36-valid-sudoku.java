27d5b6ecd7b8c65d8cbd9d82d39cf9922321bf3e
Julian Lopez <66705096+julianlopez6850@users.noreply.github.com>
2022-09-11 21:31:48 -0400

    Time: 3 ms (86.85%), Space: 45.1 MB (83.78%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.java
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -7,35 +7,27 @@ class Solution {
             {
                 if(board[i][j] != '.')
                 {
+                    // check rows and columns
                     for(int k = i + 1, l = j + 1; k < 9 || l < 9; k++, l++)
                     {
-                        if(k <= 8)
-                            if(board[i][j] == board[k][j])
-                            {
-                                return false;
-                            }
-                        if(l <= 8)
-                            if(board[i][j] == board[i][l])
-                            {
-                                return false;
-                            }
+                        // check row
+                        if(k <= 8 && board[i][j] == board[k][j])
+                            return false;
+                        //check column
+                        if(l <= 8 && board[i][j] == board[i][l])
+                            return false;
                     }
                     
-                    int box_i = (i / 3) * 3;
-                    int box_j = (j / 3) * 3;
-                    
+                    // check 3x3 boxes
+                    int box_i = (i / 3) * 3, box_j = (j / 3) * 3;
                     for(int k = box_i; k < box_i + 3;k++)
                     {
                         for(int l = box_j; l < box_j + 3; l++)
                         {
-                            if(i != k || j != l)
-                            {
-                                if(board[i][j] == board[k][l])
-                                    return false;
-                            }
+                            if((i != k || j != l) && board[i][j] == board[k][l])
+                                return false;
                         }
                     }
-                    
                 }
             }
         }
