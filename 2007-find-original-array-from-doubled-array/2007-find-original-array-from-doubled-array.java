c22bf6f163f6134e3417b888c519200dec597c1e
Julian Lopez <66705096+julianlopez6850@users.noreply.github.com>
2022-09-15 02:13:33 -0400

    Time: 56 ms (88.47%), Space: 121.7 MB (87.57%) - LeetHub

---
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[] findOriginalArray(int[] changed)
+    {
+        if(changed.length % 2 == 1)
+            return new int[0];
+        
+        int result[] = new int[changed.length / 2];
+        int resIndex = 0;
+        int prevDoubleIndex = 0;
+        
+        Arrays.sort(changed);
+        
+        for(int i = 0; i < changed.length; i++)
+        {
+            if(changed[i] != -1)
+            {
+                for(int j = (i > prevDoubleIndex) ? i + 1 : prevDoubleIndex + 1; j < changed.length; j++)
+                {
+                    if(changed[j] == changed[i] * 2)
+                    {
+                        result[resIndex] = changed[i];
+                        resIndex++;
+                        changed[j] = -1;
+                        prevDoubleIndex = j;
+                        break;
+                    }
+                    else if(j == changed.length - 1)
+                        return new int[0];
+                }
+            }
+            if(resIndex == result.length)
+                break;
+            else if(i == changed.length - 1)
+                return new int[0];
+        }
+        return result;
+    }
+}
