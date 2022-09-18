7bad4ed15f2e7ca7c0d910ab72e31f9519e8439d
Julian Lopez <66705096+julianlopez6850@users.noreply.github.com>
2022-09-18 17:13:05 -0400

    Time: 1 ms (99.76%), Space: 42.9 MB (95.07%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -21,11 +21,9 @@ class Solution {
         
         for(int i = 0; i < height.length; i++)
         {
-            if(leftSide[i] != 0 && rightSide[i] != 0)
-            {
-                if(height[i] < (min = ((leftSide[i] > rightSide[i]) ? rightSide[i] : leftSide[i])))
-                    result += min - height[i];
-            }
+            min = ((leftSide[i] > rightSide[i]) ? rightSide[i] : leftSide[i]);
+            if(min != 0 && height[i] < min)
+                result += min - height[i];
         }
         return result;
     }
