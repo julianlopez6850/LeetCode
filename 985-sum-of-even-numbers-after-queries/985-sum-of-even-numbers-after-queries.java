31f727e735fe140374d192486afe92a4defd6c9c
Julian Lopez <66705096+julianlopez6850@users.noreply.github.com>
2022-09-22 17:55:48 -0400

    Time: 16 ms (34.79%), Space: 69 MB (69.95%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
@@ -2,26 +2,26 @@ class Solution {
     public int[] sumEvenAfterQueries(int[] nums, int[][] queries) {
         
         int answer[] = new int[queries.length];
+        int even = 0;
         
         for(int i = 0; i < nums.length; i++)
         {
             if(nums[i] % 2 == 0)
-                answer[0] += nums[i];
+                even += nums[i];
         }
         
         for(int i = 0; i < queries.length; i++)
         {
             int index = queries[i][1];
             if(nums[index] % 2 == 0)
-                answer[i] -= nums[index];
+                even -= nums[index];
             
             nums[index] += queries[i][0];
             
             if(nums[index] % 2 == 0)
-                answer[i] += nums[index];
+                even += nums[index];
                 
-            if(i + 1 != queries.length)
-                answer[i+1] = answer[i];
+            answer[i] = even;
         }
         return answer;
     }
