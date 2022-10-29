a3cb5aa8a8d5a3d32616012b3c17ab0b27697e8e
Julian Lopez <66705096+julianlopez6850@users.noreply.github.com>
2022-10-28 20:45:06 -0400

    Time: 0 ms (100.00%), Space: 40.2 MB (91.75%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
@@ -1,38 +1,19 @@
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
         
-        int l = 0;
-        for(int i = 0, j = 0; i < word1.length; i++)
+        StringBuilder combined1 = new StringBuilder();
+        StringBuilder combined2 = new StringBuilder();
+        
+        for(String i : word1)
         {
-            for(int k = 0; k < word1[i].length(); k++, l++)
-            {
-                if(j == word2.length)
-                    return false;
-                if(word1[i].charAt(k) != word2[j].charAt(l))
-                    return false;
-                if(l == word2[j].length() - 1)
-                {
-                    l = -1;
-                    j++;
-                }
-            }
+            combined1.append(i);
         }
-        l = 0;
-        for(int i = 0, j = 0; i < word2.length; i++)
+        for(String i : word2)
         {
-            for(int k = 0; k < word2[i].length(); k++, l++)
-            {
-                if(j == word1.length)
-                    return false;
-                if(word2[i].charAt(k) != word1[j].charAt(l))
-                    return false;
-                if(l == word1[j].length() - 1)
-                {
-                    l = -1;
-                    j++;
-                }
-            }
+            combined2.append(i);
         }
-        return true;
+        if(combined1.toString().equals(combined2.toString()))
+            return true;
+        return false;
     }
 }
